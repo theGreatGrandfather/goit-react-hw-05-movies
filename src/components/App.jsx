770fbb5e9f3e4742350f542efe95cc04cc68b758
Praded3 <pradid.store@gmail.com>
2023-09-03 22:01:14 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate  } from "react-router-dom";
 import { lazy } from "react";
 
 import Layout from "./Layout/Layout";
@@ -12,13 +12,14 @@ const Reviews = lazy(() => import("./Reviews/Reviews"));
 export const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<Home/>} />
-        <Route path='movies' element={<Movies/>} />
-        <Route path='movies/:movieId' element={<MovieDetails />} >
-          <Route path="cast" element={<Cast/>}/>
-          <Route path="reviews" element={<Reviews/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" replace />}/>
       </Route>
     </Routes>
   );
